1f1db2cf30bfdf593706ea0b54cb5e8fecf531fa
Konstantin Terziev <kd.terziev@gmail.com>
2016-11-24 06:59:15 +0000

    Created the Relationship enumeration.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,8 +1,10 @@
 pub mod variables;
+pub mod relationships;
 
 #[cfg(test)]
 mod tests {
     use math::variables::{AbstVar, new_var, new_const};
+    use math::relationships::Relationship;
 
     #[test]
     fn can_create_variables() {
@@ -26,5 +28,15 @@ mod tests {
                    c);
         assert_eq!("barrels in stock", c.name());
         assert_eq!(450.0, c.value());
+    }
+
+    #[test]
+    fn can_create_relationships() {
+        let r: Relationship = Relationship::EQ;
+        assert_eq!(Relationship::EQ, r);
+        let r: Relationship = Relationship::LEQ;
+        assert_eq!(Relationship::LEQ, r);
+        let r: Relationship = Relationship::GEQ;
+        assert_eq!(Relationship::GEQ, r);
     }
 }
