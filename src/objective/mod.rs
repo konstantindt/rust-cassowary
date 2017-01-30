99482b8889be4262de3adbe0cf6139f7774393a0
Konstantin Terziev <kd.terziev@gmail.com>
2017-01-30 00:58:57 +0000

    Remove wrong solving for GEQ relationships.

--- src/objective/mod.rs
+++ src/objective/mod.rs
@@ -13,7 +13,7 @@ mod tests {
     use objective::functions::Function;
     use objective::constraints::Constraint;
     use objective::constraints::SystemOfConstraints;
-    use objective::solvers::{transform_geq_rels, transform_leq_rels};
+    use objective::solvers::{transform_leq_rels};
 
     #[test]
     fn can_create_problem_types() {
@@ -97,54 +97,6 @@ mod tests {
                 }
             }
         }
-    }
-
-    #[test]
-    fn can_transform_geq_rels() {
-        let e1: Expression = Expression::new(vec![new_var("x", 2.0), new_var("y", 3.0)],
-                                             Relationship::LEQ,
-                                             vec![new_const("volume", 2300.0)]);
-        let e2: Expression = Expression::new(vec![new_var("w", 6.0), new_var("z", 9.0)],
-                                             Relationship::GEQ,
-                                             vec![new_const("area", 300.0)]);
-        let c1: Constraint = Constraint::Regular(RefCell::new(e1));
-        let c2: Constraint = Constraint::Regular(RefCell::new(e2));
-        let c3: Constraint = Constraint::NonNegative(new_var("x", 2.0));
-        let s: SystemOfConstraints = SystemOfConstraints::new(vec![c1, c2, c3]);
-        transform_geq_rels(&s);
-        match s.system()[0] {
-            Constraint::Regular(ref ref_cell) => {
-                let exp = ref_cell.borrow();
-                assert_eq!("x", exp.lhs()[0].name());
-                assert_eq!(2.0, exp.lhs()[0].get_data());
-                assert_eq!(Relationship::LEQ, *exp.rel());
-                assert_eq!("y", exp.lhs()[1].name());
-                assert_eq!(3.0, exp.lhs()[1].get_data());
-                assert_eq!("volume", exp.rhs()[0].name());
-                assert_eq!(2300.0, exp.rhs()[0].get_data());
-            }
-            _ => panic!("Unexpected variant in this program logic."),
-        };
-        match s.system()[1] {
-            Constraint::Regular(ref ref_cell) => {
-                let exp = ref_cell.borrow();
-                assert_eq!("w", exp.lhs()[0].name());
-                assert_eq!(-6.0, exp.lhs()[0].get_data());
-                assert_eq!(Relationship::LEQ, *exp.rel());
-                assert_eq!("z", exp.lhs()[1].name());
-                assert_eq!(-9.0, exp.lhs()[1].get_data());
-                assert_eq!("area", exp.rhs()[0].name());
-                assert_eq!(-300.0, exp.rhs()[0].get_data());
-            }
-            _ => panic!("Unexpected variant in this program logic."),
-        };
-        match s.system()[2] {
-            Constraint::NonNegative(ref abst_var) => {
-                assert_eq!("x", abst_var.name());
-                assert_eq!(2.0, abst_var.get_data());
-            }
-            _ => panic!("Unexpected variant in this program logic."),
-        };
     }
 
     #[test]
--- src/objective/solvers.rs
+++ src/objective/solvers.rs
@@ -3,25 +3,6 @@ use math::relationships::Relationship;
 use objective::constraints::Constraint;
 use objective::constraints::SystemOfConstraints;
 
-pub fn transform_geq_rels(constraints: &SystemOfConstraints) {
-    for constraint in constraints.system() {
-        match constraint {
-            &Constraint::Regular(ref ref_cell) => {
-                let must_change: bool;
-                {
-                    let exp = ref_cell.borrow();
-                    must_change = *exp.rel() == Relationship::GEQ;
-                }
-                if must_change {
-                    let mut exp = ref_cell.borrow_mut();
-                    exp.mul_both_sides(-1.0);
-                }
-            }
-            &Constraint::NonNegative(_) => continue,
-        };
-    }
-}
-
 pub fn transform_leq_rels(constraints: &SystemOfConstraints) {
     for i in 0..constraints.system().len() {
         match constraints.system()[i] {
