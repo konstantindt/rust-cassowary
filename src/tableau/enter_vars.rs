ef90893cf7788b54582f843e75bf1c5ad9b9983a
Konstantin Terziev <kd.terziev@gmail.com>
2017-02-24 16:58:30 +0000

    Remove non-basic variable calculations. Non-basic variables have different meaning.

--- src/tableau/enter_vars.rs
+++ src/tableau/enter_vars.rs
@@ -5,8 +5,7 @@ pub fn get_enter_var_column_index(table: &Table) -> usize {
     let last_row_index = table_rows.len() - 1;
     // Select the most negative cell in the objective function row.
     let mut column_index = 0;
-    // Loop until the end of the basic variables (non-basic = number of constraints).
-    for i in 1..table_rows[last_row_index].len() - (table_rows.len() - 1) - 1 {
+    for i in 0..table_rows[last_row_index].len() - 1 {
         if table_rows[last_row_index][i] < table_rows[last_row_index][column_index] {
             column_index = i;
         }
