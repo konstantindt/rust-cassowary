df5096d58484a1ffef0eedb3d9281ba07b594f37
Konstantin Terziev <kd.terziev@gmail.com>
2017-02-25 19:32:00 +0000

    Make initial table algorithm work with rearranged function.

--- src/tableau/initials.rs
+++ src/tableau/initials.rs
@@ -24,13 +24,13 @@ pub fn get_initial_table_from(fun: &Function, constraints: &SystemOfConstraints)
             &Constraint::NonNegative(_) => continue,
         }
     }
-    // ... and don't forget about the constant on the right.
-    let map_len = column_names.len();
-    column_names.insert("RHS".to_string(), map_len);
-    for var in fun.exp().borrow().rhs() {
+    for var in fun.exp().borrow().lhs() {
         let map_len = column_names.len();
         column_names.entry(var.name().to_string()).or_insert(map_len);
     }
+    // ... and don't forget about the constant on the right.
+    let map_len = column_names.len();
+    column_names.insert("RHS".to_string(), map_len);
     let mut rows: Vec<Vec<f64>> = vec![vec![0.0; column_names.len()]; num_rows];
     // Populate the table
     let mut row_index = 0;
@@ -49,10 +49,11 @@ pub fn get_initial_table_from(fun: &Function, constraints: &SystemOfConstraints)
             &Constraint::NonNegative(_) => continue,
         }
     }
-    for var in fun.exp_max().borrow().rhs() {
+    let fun_vars = fun.exp_max().borrow();
+    for var in fun_vars.lhs() {
         rows[row_index][*column_names.get(var.name()).unwrap()] = var.get_data();
     }
     // Set the value of Z to be initially 0 in the table.
-    rows[row_index][map_len] = 0.0;
+    rows[row_index][map_len] = fun_vars.rhs()[0].get_data();
     Table::new(column_names, rows)
 }
--- src/tableau/mod.rs
+++ src/tableau/mod.rs
@@ -14,7 +14,7 @@ mod tests {
     use objective::problems::ProblemType;
     use objective::functions::Function;
     use objective::constraints::{Constraint, SystemOfConstraints};
-    use objective::solvers::transform_leq_rels;
+    use objective::solvers::{transform_leq_rels, rearrange_fun_eq_zero};
     use tableau::tables::Table;
     use tableau::initials::get_initial_table_from;
     use tableau::enter_vars::get_enter_var_column_index;
@@ -45,18 +45,20 @@ mod tests {
 
     #[test]
     fn can_create_initial_tableau() {
-        let e = Expression::new(vec![new_var("Z", 1.0)],
-                                Relationship::EQ,
-                                vec![new_var("x1", 6.0), new_var("x2", 14.0), new_var("x3", 13.0)]);
-        let f = Function::new(e, ProblemType::MAX);
-        let e1 = Expression::new(vec![new_var("x1", 0.5), new_var("x2", 2.0), new_var("x3", 1.0)],
+        let e1 =
+            Expression::new(vec![new_var("Z", 1.0)],
+                            Relationship::EQ,
+                            vec![new_var("x1", 6.0), new_var("x2", 14.0), new_var("x3", 13.0)]);
+        let mut f = Function::new(e1, ProblemType::MAX);
+        rearrange_fun_eq_zero(&mut f);
+        let e2 = Expression::new(vec![new_var("x1", 0.5), new_var("x2", 2.0), new_var("x3", 1.0)],
                                  Relationship::LEQ,
                                  vec![new_const("Metalworking (days)", 24.0)]);
-        let e2 = Expression::new(vec![new_var("x1", 1.0), new_var("x2", 2.0), new_var("x3", 4.0)],
+        let e3 = Expression::new(vec![new_var("x1", 1.0), new_var("x2", 2.0), new_var("x3", 4.0)],
                                  Relationship::LEQ,
                                  vec![new_const("Woodworking (days)", 60.0)]);
-        let c1 = Constraint::Regular(RefCell::new(e1));
-        let c2 = Constraint::Regular(RefCell::new(e2));
+        let c1 = Constraint::Regular(RefCell::new(e2));
+        let c2 = Constraint::Regular(RefCell::new(e3));
         let c3 = Constraint::NonNegative(new_var("x1", 1.0));
         let c4 = Constraint::NonNegative(new_var("x2", 1.0));
         let c5 = Constraint::NonNegative(new_var("x3", 1.0));
@@ -65,23 +67,25 @@ mod tests {
         let table = get_initial_table_from(&f, &system);
         let table_header = table.get_column_names();
         let table_rows = table.get_rows();
-        assert_eq!(6, table_header.len());
+        assert_eq!(7, table_header.len());
         assert_eq!(3, table_rows.len());
         assert!(table_header.contains_key("x1"));
         assert!(table_header.contains_key("x2"));
         assert!(table_header.contains_key("x3"));
         assert!(table_header.contains_key("s1"));
         assert!(table_header.contains_key("s2"));
+        assert!(table_header.contains_key("Z"));
         assert!(table_header.contains_key("RHS"));
         assert_eq!(0, *table_header.get("x1").unwrap());
         assert_eq!(1, *table_header.get("x2").unwrap());
         assert_eq!(2, *table_header.get("x3").unwrap());
         assert_eq!(3, *table_header.get("s1").unwrap());
         assert_eq!(4, *table_header.get("s2").unwrap());
-        assert_eq!(5, *table_header.get("RHS").unwrap());
-        assert_eq!(vec![0.5, 2.0, 1.0, 1.0, 0.0, 24.0], table_rows[0]);
-        assert_eq!(vec![1.0, 2.0, 4.0, 0.0, 1.0, 60.0], table_rows[1]);
-        assert_eq!(vec![6.0, 14.0, 13.0, 0.0, 0.0, 0.0], table_rows[2]);
+        assert_eq!(5, *table_header.get("Z").unwrap());
+        assert_eq!(6, *table_header.get("RHS").unwrap());
+        assert_eq!(vec![0.5, 2.0, 1.0, 1.0, 0.0, 0.0, 24.0], table_rows[0]);
+        assert_eq!(vec![1.0, 2.0, 4.0, 0.0, 1.0, 0.0, 60.0], table_rows[1]);
+        assert_eq!(vec![-6.0, -14.0, -13.0, 0.0, 0.0, 1.0, 0.0], table_rows[2]);
     }
 
     #[test]
